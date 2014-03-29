b208ab1c73cf326eb681aea28e8dca0e837782f9
Ghan Patel <ghanpatel@gmail.com>
2014-03-29 16:26:23 -0400

    nav ready

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-Modernizr.load({  
-  test: Modernizr.placeholder,  
-  nope: 'oldie.js'  
-});  
+// Modernizr.load({  
+//   test: Modernizr.placeholder,  
+//   nope: 'js/oldie.js'  
+// });  
+
+$(function(){
+	$("nav a").click(function(e){
+		e.preventDefault();
+		var page = $(e.target).attr("href");
+		console.log(page);
+		$(".page").removeClass("active");
+		$(page).addClass("active");
+	});
+});
