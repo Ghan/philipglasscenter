2b4aff445eeb06d4ae7b1b14710e4dbf7c8a1da3
Ghan Patel <ghanpatel@gmail.com>
2014-03-30 01:38:36 -0400

    finishes copy

--- js/main.js
+++ js/main.js
@@ -9,6 +9,13 @@ $(function(){
 		var page = $(e.target).attr("href");
 		console.log(page);
 		$(".page").removeClass("active");
+		$(".header-container").removeClass("open");
 		$(page).addClass("active");
+	});
+	
+	$("#logo").click(function(){
+		$(".header-container").addClass("open");
+		$(".page").removeClass("active");
+		$("#home").addClass("active");
 	});
 });
